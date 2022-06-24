270dfea20c8ac050f83b3b91b4b82d53b2809cde
Akhil Nair <78647966+nairakhils@users.noreply.github.com>
2022-06-24 23:09:39 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'Article Analyzer'
+project = 'idresearch'
 copyright = '2022, A. Karekkat, A. Nair, B. Palit'
 author = 'A. Karekkat, A. Nair, B. Palit'
 root_doc = 'index'
