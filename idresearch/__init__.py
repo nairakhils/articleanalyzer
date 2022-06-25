d363e0d55a2f153066e8f03c4906625a553d9893
nairakhils <nairakhilwork@gmail.com>
2022-06-25 14:56:06 +0530

    added __init__.py, idresearch.py and LICENSE.txt

---
+++ idresearch/__init__.py
@@ -0,0 +1,7 @@
+from .idresearch import get_doc, get_ner, get_reco_df, main_abstract, reco_abstract, reco_authors, doc_stats, summarize_doc, plot_CitationCount_url, plot_CitationCount_df, plot_YearTrend_url, plot_YearTrend_df
+
+"""
+artanalyze.
+
+Package to find recommended articles using Semantic Scholar AI and analyze abstracts using NLP.
+"""
